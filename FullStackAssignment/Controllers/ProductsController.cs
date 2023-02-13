d4ca44ee09ac48c5733406f40ce3068c0b54dc08
Feride Elhennawy <40718869+FerideElhennawy@users.noreply.github.com>
2023-02-13 16:14:26 +0300

    updated

---
+++ FullStackAssignment/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FullStackAssignment.Models;
+
+namespace FullStackAssignment.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductDB db = new ProductDB();
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            List<Product> products = db.Products.ToList();
+            return View(products);
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product newProduct)
+        {
+            db.Products.Add(newProduct);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ FullStackAssignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FullStackAssignment.MvcApplication" Language="C#" %>
